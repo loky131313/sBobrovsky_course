930a580432a44a1ac10f1f501fb7a304fd3e0c7f
Maria Savina <45973502+loky131313@users.noreply.github.com>
2020-08-22 17:49:33 +0100

    Add files via upload

--- oksana.py
+++ oksana.py
@@ -1,7 +1,10 @@
 def odometer(oksana):
-    Distance = 0
-    for values in oksana:
-        speed_list = [values for i,values in enumerate(oksana) if not i%2]
-    for speed in speed_list:
-        Distance = Distance + speed
+    speed_list = [values for i,values in enumerate(oksana) if not i%2]
+    time_list = [values for i,values in enumerate(oksana) if i%2]
+    Distance = speed_list[0]*time_list[0]
+    for i in range(1, (len(speed_list))):
+        Distance += speed_list[i]*(time_list[i]-time_list[i-1])
+        i += 1
     return Distance
+
+
